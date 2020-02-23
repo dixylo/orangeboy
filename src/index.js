97584ec4c9a9a5222dec1723076a7bc2492c1891
dixylo <bixylim@gmail.com>
2020-02-23 23:31:38 +1300

    Improve CSS

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
 import "./styles.css";
 import "../assets/images/favicon.ico";
-import showRef from "./Reference";
 import media from "./Media";
 import cover from "../assets/images/cover.jpg";
 import { start, pause } from "./Control";
 
+const divBackground = document.createElement("div");
+const divContainer = document.createElement("div");
 const header = document.createElement("h1");
 const divCanvas = document.createElement("div");
 const imgCover = document.createElement("img");
 const divControl = document.createElement("div");
+const divButton = document.createElement("div");
 const btnStart = document.createElement("button");
 const btnPause = document.createElement("button");
+const divSound = document.createElement("div");
 const spnLeft = document.createElement("span");
 const volume = document.createElement("input");
 const spnRight = document.createElement("span")
 const divHowTo = document.createElement("div");
-const divReference = document.createElement("div");
-const btnReference = document.createElement("button");
-const pReference = document.createElement("p");
 
 imgCover.src = cover;
 btnPause.style.display = "none";
+divBackground.id = "divBackground";
+divContainer.id = "divContainer";
+divCanvas.id = "divCanvas";
+divControl.id = "divControl";
+divHowTo.id = "divHowTo";
+divSound.id = "divSound";
 header.innerText = "Eric the Orange Boy";
 btnStart.innerText = "Start Game";
 btnPause.innerText = "Pause Game";
-spnLeft.innerText = "Sound Volume: Min";
+spnLeft.innerText = "Sound: Min";
 spnRight.innerText = "Max";
-divHowTo.innerText = "Hint: Use Up Down Left Right arrows to control Eric.";
-btnReference.innerText = "Show Reference";
+divHowTo.innerText = "--- Use Arrow keys to move Eric ---";
 
 volume.id = "volume";
 volume.type = "range";
@@ -36,19 +41,19 @@ volume.max = "100";
 
 btnStart.addEventListener("click", () => start(divCanvas, btnStart, btnPause));
 btnPause.addEventListener("click", () => pause(btnPause));
-btnReference.addEventListener("click", () => showRef(btnReference, pReference));
 volume.addEventListener("change", media.adjustVolume);
 
 divCanvas.appendChild(imgCover);
-divControl.appendChild(btnStart);
-divControl.appendChild(btnPause);
-divControl.appendChild(spnLeft);
-divControl.appendChild(volume);
-divControl.appendChild(spnRight);
-divReference.appendChild(btnReference);
-divReference.appendChild(pReference);
-document.body.appendChild(header);
-document.body.appendChild(divCanvas);
-document.body.appendChild(divControl);
-document.body.appendChild(divHowTo);
-document.body.appendChild(divReference);
+divButton.appendChild(btnStart);
+divButton.appendChild(btnPause);
+divSound.appendChild(spnLeft);
+divSound.appendChild(volume);
+divSound.appendChild(spnRight);
+divControl.appendChild(divButton);
+divControl.appendChild(divSound);
+divContainer.appendChild(header);
+divContainer.appendChild(divCanvas);
+divContainer.appendChild(divHowTo);
+divContainer.appendChild(divControl);
+document.body.appendChild(divContainer);
+document.body.appendChild(divBackground);
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,9 @@ module.exports = {
     }, {
       test: /\.(png|jpg|svg|gif|ico|mp3)$/,
       use: ['file-loader']
+    }, {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: ['file-loader']
     }]
   }
 };
